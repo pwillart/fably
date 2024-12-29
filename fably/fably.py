f33572fb6aac525b40895715443b489e7e99d50f
Patrick Willart <patrick.willart@mjfreeway.com>
2024-12-29 09:14:47 -0800

    FB-11-Default-Volume

--- fably/fably.py
+++ fably/fably.py
@@ -282,7 +282,7 @@ def main(ctx, query=None):
     logging.info("amixer - %s: %s.", result.returncode, result.stderr)
 
     result2 = subprocess.run(["/usr/bin/amixer", "-c", "3", "-D", "pulse", "sset", "Master", "65%"], capture_output=True, text=True)
-    logging.info("amixer - %s: %s.", result2.returncode, result2stderr)
+    logging.info("amixer - %s: %s.", result2.returncode, result2.stderr)
 
     ctx.stt_client = openai.Client(base_url=ctx.stt_url, api_key=ctx.api_key, )
     ctx.llm_client = openai.AsyncClient(base_url=ctx.llm_url, api_key=ctx.api_key)
